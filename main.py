bc999f69e4ef743e331730394b9422760a3d919b
alisherqobilov383-afk <alisherqobilov383@gmail.com>
2026-06-09 09:56:47 +0500

    Refactor bot code and improve message handling

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def start_bot():
     # Manbalar va Targetlar (ENV dan olinadi)
     S1, T1 = os.environ.get("SOURCE_1", "@eltuzar_live"), os.environ.get("TARGET_1", "@eltuzar_livee")
     S2, T2 = os.environ.get("SOURCE_2", "@tuztuzttt"), os.environ.get("TARGET_2", "@eltuzar_mediaa")
-    S3, T3 = os.environ.get("SOURCE_3", "@tuztuzttt"), os.environ.get("TARGET_3", "@wergfdgsdfsfwerw")
+    S3, T3 = os.environ.get("SOURCE_3", ""), os.environ.get("TARGET_3", "@wergfdgsdfsfwerw")
 
     app = Client("render_userbot", api_id=int(api_id), api_hash=api_hash, session_string=session_string)
 
